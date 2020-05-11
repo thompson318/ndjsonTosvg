f0a3ef8be05c70d17788e376972d5aac4fe2f71b
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-05-11 15:24:05 +0100

    Issue #1 tidied up

--- ndjsontosvg/ndjsontosvg.py
+++ ndjsontosvg/ndjsontosvg.py
@@ -1,26 +1,30 @@
 #! /usr/bin/python3
-
-import ndjson
-import random
+"""converts simplified quickdraw into separate svg files"""
 import os
+import random
 import ntpath
+import ndjson
 
-def _write_header(f, out_size, key_id):
-    """ 
-    Writes the svg header to file f 
+def _write_header(f_out, out_size, key_id):
+    """
+    Writes the svg header to file f
 
     :params f: the file to write to
     :params out_size: the size of the svg image to write
     :params key_id: the key_id taken from the ndjson file
     """
-    f.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
-    f.write('<!--Created with ndjsontosvg (https:https://github.com/thompson318/ndjsontosvg) \n')
-    f.write('\tfrom the simplified Google quickdraw data set. key_id = {:s}-->\n'.format(key_id))
-    f.write('<svg width="{:d}" height="{:d}"\n'.format(out_size, out_size))
-    f.write('\txmlns="http://www.w3.org/2000/svg">\n')
-    f.write('\txmlns:ndjsontosvg="https://github.com/thompson318/ndjsontosvg"\n')
-    f.write('\txmlns:quickdraw="https://quickdraw.withgoogle.com/data"\n')
-    f.write('\txmlns:scikit-surgery="https://doi.org/10.1007/s11548-020-02180-5">\n\n')
+    f_out.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n')
+    f_out.write('<!--Created with ndjsontosvg')
+    f_out.write('(https:https://github.com/thompson318/ndjsontosvg) \n')
+    f_out.write('\tfrom the simplified Google quickdraw data set. ')
+    f_out.write('key_id = {:s}-->\n'.format(key_id))
+    f_out.write('<svg width="{0:d}" height="{0:d}"\n'.format(out_size))
+    f_out.write('\txmlns="http://www.w3.org/2000/svg"\n')
+    f_out.write('\txmlns:')
+    f_out.write('ndjsontosvg="https://github.com/thompson318/ndjsontosvg"\n')
+    f_out.write('\txmlns:quickdraw="https://quickdraw.withgoogle.com/data"\n')
+    f_out.write('\txmlns:scikit-surgery=')
+    f_out.write('"https://doi.org/10.1007/s11548-020-02180-5"/>\n\n')
 
 
 def _draw_line(fileout, line, colour, scale):
@@ -34,71 +38,78 @@ def _draw_line(fileout, line, colour, scale):
 
     for point_index, x_ord in enumerate(x_ordinates):
         if point_index == 0:
-            fileout.write('\t<path d = "M {:.2f} {:.2f}'.format(x_ord * scale, y_ordinates[point_index] * scale))
+            fileout.write(
+                '\t<path d = "M {:.2f} {:.2f}'.format(
+                    x_ord * scale, y_ordinates[point_index] * scale))
         else:
-            fileout.write(' L {:.2f} {:.2f}'.format(x_ord * scale, y_ordinates[point_index] * scale))
+            fileout.write(' L {:.2f} {:.2f}'.format(
+                x_ord * scale, y_ordinates[point_index] * scale))
     fileout.write('" stroke="{:s}" fill="transparent"/>\n'.format(colour))
 
 
 
-def ndjsontosvg(filein, outsize , numberofsamples,
-                 linecolour='black', backgroundcolour='white', 
-                 outdir = "./",
-                 checkifidentified = True, randomsort = True,
-                 inputsize = 256):
-    
-    print("filein =  " + filein)
-    print("outsize =  " + str(outsize))
-    print("ns =  " + str(numberofsamples))
-    print("outdir =  " + outdir)
+def ndjsontosvg(filein, numberofsamples, outsize=256,
+                linecolour='black', backgroundcolour='white',
+                outdir="./",
+                checkifidentified=True, randomsort=True,
+                inputsize=256):
+    """
+    converts a multiline google quickdraw simplified format file into
+    separate svg images.
+    :params filein: the input json file
+    :params numberofsamples: how many drawings to generate
+    :params outsize: You can set the outsize, simplified quickdraw
+        are designed for 256 x 256, and svg should scale easily, but for
+        some applications that don't scale svg well, you can set
+        your own output size.
+    :params outdir: the directory to write to.
+    :params checkifidenified: if true we will check that recognized key is
+        true before creating svg.
+    :params randomsort: If true we will select a random set of drawings,
+        not just the first numberofsamples
+    :params inputsize: use this if the input ndjson is not 256x256
+
+    :raises ValueError: If more that 10000 samples requested.
+    :raises KeyError: If ndjson is missing expected fields.
+    """
     if numberofsamples > 10000:
-       raise ValueError("Maximum number_of_samples is 10000")
-    
-    fileoutprefix=os.path.splitext(ntpath.basename(filein))[0]
-    print("fileoutprefix =  " + fileoutprefix)
+        raise ValueError("Maximum number_of_samples is 10000")
+
+    fileoutprefix = os.path.splitext(ntpath.basename(filein))[0]
     scale = outsize / inputsize
     data = None
-    with open(filein) as f:
-        data = ndjson.load(f)
+    with open(filein) as f_in:
+        data = ndjson.load(f_in)
 
     if randomsort:
-        random.shuffle(data);
-    samples = 0;
-    
+        random.shuffle(data)
+    samples = 0
+
     for item in data:
-        if checkifidentified:    
+        if checkifidentified:
             if 'recognized' not in item:
                 raise KeyError("Item has no recognized key")
             if not item.get('recognized'):
-                continue    
+                continue
 
         if 'drawing' not in item:
             raise KeyError("Item has no drawing data")
-        
+
         if 'key_id' not in item:
             raise KeyError("Item has no key_id data")
-        
-        outfilename = os.path.join(outdir, fileoutprefix +  '_{:04d}.svg'.format(samples))
 
-        with open(outfilename, 'w') as f:
+        outfilename = os.path.join(
+            outdir, fileoutprefix +  '_{:04d}.svg'.format(samples))
+
+        with open(outfilename, 'w') as f_out:
             drawing = item.get('drawing')
-            _write_header(f, outsize, item.get('key_id'))
-            f.write('\t<rect width="100%" height="100%" fill="{:s}" />\n'.format(backgroundcolour))
+            _write_header(f_out, outsize, item.get('key_id'))
+            f_out.write('\t<rect width="100%" height="100%" ')
+            f_out.write('fill="{:s}" />\n'.format(backgroundcolour))
             for line in drawing:
-                _draw_line(f, line, linecolour, scale)
-            f.write('</svg>')
-        
+                _draw_line(f_out, line, linecolour, scale)
+            f_out.write('</svg>')
+
         samples += 1
         if samples >= numberofsamples:
             break
-
-
-if __name__ == '__main__':
-    insize = 256  #this is from the simplified data set
-    outsize = 600 #this is what we want it to be, it's easier to set it in the image, as phaser doesn't scale svg well
-    linecolour = "black"
-    backgroundcolour = "white"
-    numberofsamples = 100
-    outdir = "white_on_black/"
-    ndjsontosvns('hot air balloon.ndjson', outsize, numberofsamples, linecolour, backgroundcolour,
-                    outdir )
--- ndjsontosvg/ui/ndjsontosvg_command_line.py
+++ ndjsontosvg/ui/ndjsontosvg_command_line.py
@@ -1,12 +1,10 @@
 # coding=utf-8
 
-"""Command line processing"""
-
+"""Command line ui for ndjsontosvg"""
 
 import argparse
 from ndjsontosvg import __version__
 from ndjsontosvg.ndjsontosvg import ndjsontosvg
-
 
 def main(args=None):
     """Entry point for ndjsonTosvg application"""
@@ -19,22 +17,22 @@ def main(args=None):
                         type=str,
                         help="the input file (ndjson format)")
 
-    parser.add_argument("-s", "--outsize",
-                        required=True,
-                        type=int,
-                        help="The desired out put size (pixels)")
-    
     parser.add_argument("-n", "--numberofsamples",
                         required=True,
                         type=int,
                         help="The number of samples to write out.")
 
     # ADD OPTINAL ARGUMENTS
+    parser.add_argument("-s", "--outsize",
+                        default=256,
+                        type=int,
+                        help="The desired out put size (pixels)")
+
     parser.add_argument("-lc", "--linecolour",
                         default="black",
                         help="The line colour to use."
                         )
-    
+
     parser.add_argument("-bc", "--backgroundcolour",
                         default="white",
                         help="The background colour to use."
@@ -47,20 +45,23 @@ def main(args=None):
 
     parser.add_argument("-ci", "--checkifidentified",
                         action="store_true",
-                        help="Only select entries that were recognized by google AI",
+                        help="Only select entries that were recognized by" + \
+                            "google AI",
                         )
 
     parser.add_argument("-rs", "--randomsort",
                         action="store_true",
-                        help="Make a random selection, rather than the first n lines",
+                        help="Make a random selection, rather than the" + \
+                            "first n lines",
                         )
 
 
     parser.add_argument("-is", "--inputsize",
                         default=256,
-                        help="The input image size, 256 pixels for the simplified quickdraw data set"
+                        help="The input image size, 256 pixels for the " +\
+                            "simplified quickdraw data set"
                         )
-    
+
     version_string = __version__
     friendly_version_string = version_string if version_string else 'unknown'
     parser.add_argument(
@@ -70,6 +71,7 @@ def main(args=None):
 
     args = parser.parse_args(args)
 
-    ndjsontosvg(args.filein, args.outsize, args.numberofsamples, args.linecolour,
-                    args.backgroundcolour, args.outdir, args.checkifidentified,
-                    args.randomsort, args.inputsize)
+    ndjsontosvg(args.filein, args.numberofsamples, args.outsize,
+                args.linecolour,
+                args.backgroundcolour, args.outdir, args.checkifidentified,
+                args.randomsort, args.inputsize)
